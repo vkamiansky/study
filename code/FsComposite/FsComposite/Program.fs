c25307c66c46093af5a2d09f22ee69942fa47ead
Vladimir Ilyin <vladimir.ilyin@firstlinesoftware.com>
2017-06-30 22:47:44 +0300

    Added possibility to search elements of specific type in lazylist

--- code/FsComposite/FsComposite/Program.fs
+++ code/FsComposite/FsComposite/Program.fs
@@ -3,6 +3,8 @@
 open System
 open CompositeToolset
 
+open ObservableSource
+
 module Program =
 
     let rec toString obj =
@@ -19,15 +21,46 @@ module Program =
     type Simple =
        | A
        | B
+       | C
+
 
     let transform obj =
        match obj with
        | A -> l B
        | x -> l x
 
+    let rec produce_tuple_smart t o f =
+        match t with
+        |(None, _) | (_, None) -> match o with
+                                  | Nil -> t
+                                  | Cons(h, tail) -> produce_tuple_smart (f h t) tail f
+        | _ -> t
+
+
+    let transform_ab_smart obj =
+        let f h t=
+            match h, t with
+            | B, (a, _) -> (a, Some B)
+            | A, (_, b) -> (Some A, b)
+            | _ -> t
+        match produce_tuple_smart (None, None) obj f with
+        | (None, _) | (_, None) -> LazyList.empty
+        | (a, b) -> l [C]
+
     [<EntryPoint>]
-    let main argv = 
-        let f = ana [ v transform ] (Composite ([Value A; Value A]|> LazyList.ofList)) |> flat
+    let main argv =
+
+        // Create a source.
+//        let source = new ObservableSource<'a Composite>()
+
+        // Get an IObservable from the source.
+//        let obs = source.AsObservable
+
+//        obs |> Observable.subscribe(fun c -> ana [v transform] c |> toConsole |> ignore) |> ignore
+        
+        let ccc = transform_ab_smart ([B; C] |> LazyList.ofList) |> Array.ofSeq
+
+        let f = ana [ v transform ] (Composite ([Value A; Value A]|> LazyList.ofList))
         
         f |> toConsole
 
