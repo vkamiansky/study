7e41635aa7a7189fc05fcc2375b5bb4c5c7fc361
Vladimir Ilyin <vladimir.ilyin@firstlinesoftware.com>
2017-06-30 14:21:29 +0300

    added function for anamorphism and groundwork for catamorphism

--- code/FsComposite/FsComposite/CompositeToolset.fs
+++ code/FsComposite/FsComposite/CompositeToolset.fs
@@ -2,6 +2,40 @@
 
 module CompositeToolset = 
 
+    exception FatalError of string
+
     type 'a Composite =
     | Value of 'a
-    | Composite of seq<Composite<'a>>
+    | Composite of LazyList<Composite<'a>>
+
+    let to_composite obj =
+        match obj with
+        | Nil -> raise(FatalError "Empty data sequence will not lead to a meaningful Composite instance.")
+        | Cons(x, Nil) -> Value x
+        | x -> Composite(LazyList.map Value x)
+
+    let rec ana scn obj =
+        match scn with
+        | [] -> obj
+        | f :: scn_tail -> match obj with
+                           | Value x -> ana scn_tail (to_composite(f x))
+                           | Composite x -> Composite(LazyList.map (ana scn) x)
+
+    let l obj =
+        [obj] |> LazyList.ofList    
+
+    let v f obj =
+        match f obj with
+        | Nil -> raise(FatalError "Empty data sequence is an invalid binding result.")
+        | Cons(x, Nil) -> if x = obj then l x else [obj; x] |> LazyList.ofList
+        | x -> LazyList.cons obj x
+
+    let flat obj =
+        let rec flat_inner o =
+            match o with
+            | Composite x -> LazyList.collect flat_inner x
+            | Value x -> l x
+
+        match obj with
+        | Value x -> obj
+        | Composite x -> Composite (LazyList.map (fun y -> Composite(LazyList.map Value (flat_inner y)) ) x)
--- code/FsComposite/FsComposite/Program.fs
+++ code/FsComposite/FsComposite/Program.fs
@@ -11,9 +11,27 @@ module Program =
              "(" + String.Join(", ", Seq.map toString x) + ")"
         | Value x -> sprintf "%A" x
 
+    let rec toConsole obj =
+        match obj with
+        | Composite x -> printf "%s" "("; LazyList.iter toConsole x |> ignore; printf "%s" ")";
+        | Value x -> printf "%A " x
+
+    type Simple =
+       | A
+       | B
+
+    let transform obj =
+       match obj with
+       | A -> l B
+       | x -> l x
+
     [<EntryPoint>]
     let main argv = 
-        let comp = Composite([Value 1; Composite([Value 2; Value 3; Composite([Value 4])]);])
-        printfn "%s" (toString comp)
-        Console.ReadKey() |> ignore 
+        let f = ana [ v transform ] (Composite ([Value A; Value A]|> LazyList.ofList)) |> flat
+        
+        f |> toConsole
+
+        //f |> toString |> printf "%A"
+
+        Console.ReadKey() |> ignore
         0 // return an integer exit code
