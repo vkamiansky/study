36303fd0b60add5081b6de4675b863c58ba8209c
Vladimir Ilin <xrigikx@mail.ru>
2017-07-07 00:45:56 +0300

    Started making the function for transforming from 3 entities into 1

--- code/FsComposite/Composite.Core/Processing.fs
+++ code/FsComposite/Composite.Core/Processing.fs
@@ -10,6 +10,13 @@ module Processing =
                                     | Nil -> acc
                                     | Cons(h, tail) -> fold_2 (f_update_acc h acc) f_update_acc tail
         | _ -> acc
+        
+    let rec fold_3 acc f_update_acc lst =
+        match acc with
+        |(None, _) | (_, None) -> match lst with
+                                    | Nil -> acc
+                                    | Cons(h, tail) -> fold_3 (f_update_acc h acc) f_update_acc tail
+        | _ -> acc
 
     let find_2_and_transform f_is_1 f_is_2 f_transform lst =
         let f h t =
@@ -23,3 +30,13 @@ module Processing =
     let find_2_and_transform_strict f_is_1 f_is_2 f_transform lst =
         let f_transform_strict = function | (Some x1, Some x2) -> l(f_transform (x1, x2)) | _ ->  LazyList.empty
         find_2_and_transform f_is_1 f_is_2 f_transform_strict lst
+
+    let find_3_and_transform f_is_1 f_is_2 f_is_3 f_transform lst =
+        let f h t =
+            match f_is_1 h, f_is_2 h, f_is_3 h, t with
+            | _, _, true, (a, b, None) -> (a, b, Some h)
+            | _, true, _, (a, None, b) -> (a, Some h, b)
+            | true, _, _, (None, a, b) -> (Some h, a, b)
+            | _ -> t
+        match fold_3 (None, None, None) f lst with
+        | x -> f_transform x
