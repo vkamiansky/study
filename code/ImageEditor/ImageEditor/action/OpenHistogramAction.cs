0e3259f5d8db30a1c5670165d9c6d77d67cfe065
Grigory Zaripov <grigory.zaripov@firstlinesoftware.com>
2017-08-07 17:57:29 +0300

    add Histogram menu item

---
+++ code/ImageEditor/ImageEditor/action/OpenHistogramAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Interactivity;
+
+namespace ImageEditor.action
+{
+    public class OpenHistogramAction : TriggerAction<FrameworkElement>
+    {
+        protected override void Invoke(object parameter)
+        {
+            
+        }
+    }
+}
