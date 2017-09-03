4fe0db402819f5c16f3090ba16113381acf7bcbf
Vladimir Ilin <xrigikx@mail.ru>
2017-09-03 17:39:57 +0300

    Added the password field to the config template.

--- code/FsComposite/Composite.Test/DataSourceConfigurationManager.fs
+++ code/FsComposite/Composite.Test/DataSourceConfigurationManager.fs
@@ -7,12 +7,17 @@ open Newtonsoft.Json.Linq
 
 module DataSourceConfigurationManager =
 
+    let getJsonValue (config: JObject) key =
+        config.SelectToken(sprintf "$.%s" key) |> string
+    
     module Github =
         
         let config_path = ConfigurationManager.AppSettings.["GithubConfigPath"]
 
         type Config () =
             let config = JObject.Parse(File.ReadAllText(config_path))
+            let getJsonValueFromConfig = getJsonValue config
 
-            member this.Username = config.SelectToken("$.user") |> string
-            member this.Repository = config.SelectToken("$.repository") |> string
+            member this.Username = getJsonValueFromConfig "user"
+            member this.Password = getJsonValueFromConfig "password"
+            member this.Repository = getJsonValueFromConfig "repository"
